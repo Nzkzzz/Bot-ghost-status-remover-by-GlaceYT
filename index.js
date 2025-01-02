c9ae1360f435c6ef8bd5ef503bfcda5365b9261b
Nzkzzz <victorhidekiseki@gmail.com>
2025-01-02 23:24:47 +0900

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["✨ HAPPY 2025!", "✨ HAPPY 2025!", "✨ HAPPY 2025!"];
+const statusMessages = ["📢 Game coming soon", "📢 Game coming soon", "📢 Game coming soon"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
